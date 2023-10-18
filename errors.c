42bb8b6ec823b31d8bfebe9006ab4e26b4e8e121
“Tenawerk” <zelle.helena@gmail.com>
2023-10-18 15:35:34 -0700

    commit

---
+++ errors.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints input string
+ * @str: print string
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write character c to stderr
+ * @c: character to print
+ *
+ * Return: success = 1.
+ * On error, -1 = returned, and errno is set appropriately.
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes character c to given fd
+ * @c: Print character
+ * @fd: The file descriptor to write to
+ *
+ * Return: success = 1.
+ * On error, -1 = returned, and errno is set appropriately.
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints input string
+ * @str: string to be printed
+ * @fd: the file descriptor to write to
+ *
+ * Return: the amount of chars put
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
